ed8543df6136ff2f588a2e445f8327afdd4de11a
王晓丽 <wangxiaoli@troila.com>
2019-01-16 14:38:36 +0800

    切换菜单

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,20 @@ var myFullpage = new fullpage("#fullpage", {
 	//导航
 	menu: "#menu",
 	lockAnchors: false,
-	anchors: ["firstPage", "secondPage"],
-	sectionsColor: ["#000000cc", "#ffaaaa88", "#ccffff88", "#ffaadd88"]
+	anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage"],
+	sectionsColor: [
+		"#000000cc",
+		"#3f5f9acc",
+		"#993399cc",
+		"#ffaaddcc",
+		"#000000cc"
+	],
+	//切换菜单
+	onLeave: function(origin, destination) {
+		const $menu = $(".menu");
+		const index = destination.index;
+
+		$menu.removeClass("active");
+		$menu[index].classList.add("active");
+	}
 });
